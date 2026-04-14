ab857eee64a51dd387fcd81efb976ab0ba5c39be
ibraheemrashid2025 <ibraheemrashid65@gmail.com>
2026-04-14 17:45:55 +0500

    Live demo test

--- src/train.py
+++ src/train.py
@@ -6,18 +6,16 @@ import mlflow
 mlflow.set_tracking_uri("http://localhost:5000")
 import mlflow.sklearn
 
-# 1. Data Ingest [cite: 12]
 iris = load_iris()
 X = pd.DataFrame(iris.data, columns=iris.feature_names)
 y = iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# 2. Model Train [cite: 13]
 with mlflow.start_run():
     rf = RandomForestClassifier(n_estimators=100)
     rf.fit(X_train, y_train)
     
-    # Model Log karna
     mlflow.sklearn.log_model(rf, "iris_model")
     print("Model trained and logged to MLflow!")
+    # Live demo for presentation
